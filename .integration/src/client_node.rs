3396e018aa5296c98217c216c1681defaebbd727
ljedrz <ljedrz@gmail.com>
2022-04-07 11:13:02 +0200

    refactor: introduce a State object
    Signed-off-by: ljedrz <ljedrz@gmail.com>

--- .integration/src/client_node.rs
+++ .integration/src/client_node.rs
@@ -33,13 +33,13 @@ impl ClientNode {
 
     /// Returns the list of connected peers of the node.
     pub async fn connected_peers(&self) -> Vec<SocketAddr> {
-        self.server.peers().connected_peers().await
+        self.server.state.peers().connected_peers().await
     }
 
     /// Resets the node's known peers. This is practical, as it makes the node not reconnect
     /// to known peers in test cases where it's undesirable.
     pub async fn reset_known_peers(&self) {
-        self.server.peers().reset_known_peers().await
+        self.server.state.peers().reset_known_peers().await
     }
 
     /// Attempts to connect the node to the given address.
