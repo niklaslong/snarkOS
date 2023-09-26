ec313790b81d0d2f7c1aa02edb00cc2f742b3d7f
Niklas <niklas@long.ch>
2023-09-26 11:29:07 +0200

    feat: detect duplicate noise state in handshake

--- node/narwhal/src/gateway.rs
+++ node/narwhal/src/gateway.rs
@@ -54,7 +54,17 @@ use snarkos_node_tcp::{
 
 use snarkvm::{console::prelude::*, ledger::narwhal::Data, prelude::Address};
 
-use std::{collections::HashMap, future::Future, io, net::SocketAddr, sync::Arc, time::Duration};
+use std::{
+    collections::{
+        hash_map::Entry::{Occupied, Vacant},
+        HashMap,
+    },
+    future::Future,
+    io,
+    net::SocketAddr,
+    sync::Arc,
+    time::Duration,
+};
 
 use bytes::Bytes;
 use colored::Colorize;
@@ -1009,16 +1019,13 @@ impl<N: Network> Handshake for Gateway<N> {
             self.handshake_inner_responder(peer_addr, &mut peer_ip, stream).await
         };
 
-        // Remove the address from the collection of connecting peers (if the handshake got to the point where it's known).
+        // Remove the address from the collection of connecting peers if the handshake got to the point where it's known,
+        // even if the entire process has failed in the end.
         if let Some(ip) = peer_ip {
             self.connecting_peers.lock().shift_remove(&ip);
         }
 
-        let (ref peer_ip, framed) = handshake_result?;
-        let FramedParts { codec, .. } = framed.into_parts();
-        let NoiseCodec { noise_state, .. } = codec;
-        self.noise_states.write().insert(*peer_ip, noise_state);
-
+        let peer_ip = handshake_result?;
         info!("{CONTEXT} Gateway is connected to '{peer_ip}'");
 
         Ok(connection)
@@ -1084,7 +1091,7 @@ impl<N: Network> Gateway<N> {
         peer_addr: SocketAddr,
         peer_ip: Option<SocketAddr>,
         stream: &'a mut TcpStream,
-    ) -> io::Result<(SocketAddr, Framed<&mut TcpStream, NoiseCodec<N>>)> {
+    ) -> io::Result<SocketAddr> {
         // This value is immediately guaranteed to be present, so it can be unwrapped.
         let peer_ip = peer_ip.unwrap();
 
@@ -1160,10 +1167,14 @@ impl<N: Network> Gateway<N> {
         let our_response = ChallengeResponse { signature: Data::Object(our_signature) };
         send_event(&mut framed, peer_addr, Event::ChallengeResponse(our_response)).await?;
 
+        // Save the noise state; this must happen before marking the peer as "connected", otherwise the node
+        // won't have the means to encrypt its messages sent to it.
+        self.save_noise_state(peer_ip, framed)?;
+
         // Add the peer to the gateway.
         self.insert_connected_peer(peer_ip, peer_addr, peer_request.address);
 
-        Ok((peer_ip, framed))
+        Ok(peer_ip)
     }
 
     /// The connection responder side of the handshake.
@@ -1172,7 +1183,7 @@ impl<N: Network> Gateway<N> {
         peer_addr: SocketAddr,
         peer_ip: &mut Option<SocketAddr>,
         stream: &'a mut TcpStream,
-    ) -> io::Result<(SocketAddr, Framed<&mut TcpStream, NoiseCodec<N>>)> {
+    ) -> io::Result<SocketAddr> {
         /* Noise codec setup */
 
         let params: NoiseParams = NOISE_HANDSHAKE_TYPE.parse().unwrap();
@@ -1252,10 +1263,34 @@ impl<N: Network> Gateway<N> {
             send_event(&mut framed, peer_addr, reason.into()).await?;
             return Err(error(format!("Dropped '{peer_addr}' for reason: {reason:?}")));
         }
+
+        // Save the noise state; this must happen before marking the peer as "connected", otherwise the node
+        // won't have the means to encrypt its messages sent to it.
+        self.save_noise_state(peer_ip, framed)?;
+
         // Add the peer to the gateway.
         self.insert_connected_peer(peer_ip, peer_addr, peer_request.address);
 
-        Ok((peer_ip, framed))
+        Ok(peer_ip)
+    }
+
+    /// Saves the finalized noise state, checking for potential duplicate connection attempts.
+    fn save_noise_state(&self, peer_ip: SocketAddr, framed: Framed<&mut TcpStream, NoiseCodec<N>>) -> io::Result<()> {
+        match self.noise_states.write().entry(peer_ip) {
+            Occupied(_) => {
+                // Double-connect, abort.
+                Err(io::ErrorKind::AlreadyExists.into())
+            }
+            Vacant(entry) => {
+                // We are the first to handshake with the peer listener, store the noise state for
+                // reuse in Reading and Writing.
+                let FramedParts { codec, .. } = framed.into_parts();
+                let NoiseCodec { noise_state, .. } = codec;
+                entry.insert(noise_state);
+
+                Ok(())
+            }
+        }
     }
 
     /// Verifies the given challenge request. Returns a disconnect reason if the request is invalid.
